0544fa4f2abbf57bec9155f3bc418867a02f78b5
Orazbay <you@biy.kz>
2023-10-15 14:39:47 +0600

    Добавьте файлы проекта.

---
+++ Lesson6.practise/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6.practise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количетво легковых машин: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            PassengerCar[] cars = new PassengerCar[n];
+            for(int i=0; i<cars.Length; i++)
+            {
+                Console.Write("Введите марку: ");
+                string brand = Console.ReadLine();
+                Console.Write("Введите номер: ");
+                int number=Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите грузоподоемность: ");
+                double loadcapasity=Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите скорость: ");
+                double speed=Convert.ToDouble(Console.ReadLine());
+
+                PassengerCar car = new PassengerCar(brand, number, loadcapasity, speed);
+                cars[i] = car;
+            }
+            Console.WriteLine("");
+            Console.Write("Введите количетво мотоциклов: ");
+            int nM = Convert.ToInt32(Console.ReadLine());
+            Motobike[] motobikes = new Motobike[nM];
+            for (int i = 0; i < motobikes.Length; i++)
+            {
+                Console.Write("Введите марку: ");
+                string brand = Console.ReadLine();
+                Console.Write("Введите номер: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите грузоподоемность: ");
+                double loadcapasity = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите скорость: ");
+                double speed = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Есть ли коляска, если есть введите 1 если нет 0: ");
+                int b=Convert.ToInt32(Console.ReadLine());
+                bool presenceofstroller=false;
+                if (b == 1)
+                {
+                    presenceofstroller = true;
+                }
+                Motobike motobike = new Motobike(brand, number, loadcapasity, speed, presenceofstroller);
+                motobikes[i] = motobike;
+            }
+
+            Console.WriteLine("");
+            Console.Write("Введите количетво мотоциклов: ");
+            int nT = Convert.ToInt32(Console.ReadLine());
+            Truck[] trucks = new Truck[nT];
+            for (int i = 0; i < trucks.Length; i++)
+            {
+                Console.Write("Введите марку: ");
+                string brand = Console.ReadLine();
+                Console.Write("Введите номер: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите грузоподоемность: ");
+                double loadcapasity = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите скорость: ");
+                double speed = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Есть ли прицеп, если есть введите 1 если нет 0: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                bool availabilityoftrailer = false;
+                if (b == 1)
+                {
+                    availabilityoftrailer = true;
+                }
+                Truck truck = new Truck(brand, number, loadcapasity, speed, availabilityoftrailer);
+                trucks[i] = truck;
+            }
+
+
+        }
+    }
+}
---
+++ Lesson6.practise/Trans.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZendeskApi_v2.Requests;
+
+namespace Lesson6.practise
+{
+    public abstract class Trans
+    {
+        public  string brand { get; set; }
+        public int number { get; set; }
+        public double loadcapacity { get; set; }
+        public double speed { get; set; }
+        public bool presenceofstroller { get; set; }
+        public bool availabilityoftrailer { get; set; }
+
+        public abstract double Definitionofloadcapacity();
+        public abstract void info();
+    }
+    public class PassengerCar : Trans
+    {
+        public PassengerCar(string brand, int number, double loadcapacity, double speed)
+        {
+            this.brand = brand;
+            this.number = number;
+            this.loadcapacity = loadcapacity;
+            this.speed = speed;
+        } 
+        public override double Definitionofloadcapacity()
+        {
+            return loadcapacity;
+        }
+
+        public override void info()
+        {
+            Console.WriteLine("Марка: "+brand);
+            Console.WriteLine("Номер: " + number);
+            Console.WriteLine("Грузоподъемность: " + loadcapacity);
+            Console.WriteLine("Скорость: " + speed);
+        }
+
+        public void SearchLoadcapacity(PassengerCar[] passengerCars, double Loadcapacity)
+        {
+            for(int i = 0; i < passengerCars.Length; i++)
+            {
+                if (passengerCars[i].loadcapacity >= Loadcapacity)
+                {
+                    passengerCars[i].info();
+                    Console.WriteLine("");
+                }
+            }
+
+        }
+    }
+    public class Motobike : Trans
+    {
+        public Motobike(string brand, int number, double loadcapacity, double speed, bool presenceofstroller)
+        {
+            this.brand = brand;
+            this.number = number;
+            this.loadcapacity = loadcapacity;
+            this.speed = speed;
+            this.presenceofstroller = presenceofstroller;
+        }
+        public override double Definitionofloadcapacity()
+        {
+            if (presenceofstroller)
+                return loadcapacity;
+            else 
+                return 0;
+        }
+
+        public override void info()
+        {
+            Console.WriteLine("Марка: " + brand);
+            Console.WriteLine("Номер: " + number);
+            Console.WriteLine("Грузоподъемность: " + loadcapacity);
+            Console.WriteLine("Скорость: " + speed);
+            Console.WriteLine("Наличие коляски: "+presenceofstroller);
+        }
+
+        public void SearchLoadcapacity(Motobike[] motobikes, double Loadcapacity)
+        {
+            for (int i = 0; i < motobikes.Length; i++)
+            {
+                if (motobikes[i].loadcapacity >= Loadcapacity)
+                {
+                    motobikes[i].info();
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+    public class Truck : Trans
+    {
+        public Truck(string brand, int number, double loadcapacity, double speed, bool availabilityoftrailer)
+        {
+            this.brand = brand;
+            this.number = number;
+            this.loadcapacity = loadcapacity;
+            this.speed = speed;
+            this.availabilityoftrailer = availabilityoftrailer;
+        }
+        public override double Definitionofloadcapacity()
+        {
+            if(availabilityoftrailer)
+                return loadcapacity*2;
+            else
+                return loadcapacity;
+        }
+
+        public override void info()
+        {
+            Console.WriteLine("Марка: " + brand);
+            Console.WriteLine("Номер: " + number);
+            Console.WriteLine("Грузоподъемность: " + loadcapacity);
+            Console.WriteLine("Скорость: " + speed);
+            Console.WriteLine("Наличие прицепа: " + availabilityoftrailer);
+        }
+
+        public void SearchLoadcapacity(Truck[] trucks, double Loadcapacity)
+        {
+            for (int i = 0; i < trucks.Length; i++)
+            {
+                if (trucks[i].loadcapacity >= Loadcapacity)
+                {
+                    trucks[i].info();
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
